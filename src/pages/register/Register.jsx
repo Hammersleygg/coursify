052beb31bc955008271a0cf2d972ba83901a7fbb
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2023-10-16 20:51:54 -0400

    organize buttons

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -14,9 +14,10 @@ export default function Register() {
             <input type="password" className="registerInput" placeholder="Enter your password" />
             <button className="registerButton">Register</button>
       </form>
-      {/* <button className="registerLoginButton">
+      <div className="registerLoginText"> Already have an account?</div>
+      <button className="registerLoginButton">
       <Link to="/login">Login</Link>
-      </button> */}
+      </button>
     </div>
   )
 }
