af78aa1f28eb681ea73d9cc88a8e4bd7ca94952c
brandon <trappbm@g.cofc.edu>
2023-11-19 21:10:34 -0500

    added comment feature

--- src/pages/courseHomePage/CourseHomePage.jsx
+++ src/pages/courseHomePage/CourseHomePage.jsx
@@ -1,58 +1,66 @@
-//import {React} from 'react';
-import { useLocation} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getDocs, collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebase-config';
 import './courseHomePage.css';
-import firebase from 'firebase/app';
-import {db,auth } from "../../firebase-config";
-import { getDocs, collection,addDoc,doc } from "firebase/firestore";
-import { useState, useEffect } from 'react';
-import * as React from "react";
-
-
-
-
 
 const CourseHomePage = () => {
-    const [comments, setComments] = useState([]);
-    const [classInfo, setClassInfo] = useState('');
-    const location = useLocation();
-    const { selectedCourse } = location.state || {};
-  
-    useEffect(() => {
-      const getData = async () => {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'classComments'));
-          const commentsData = [];
-          querySnapshot.forEach((doc) => {
-            commentsData.push(doc.data().Comment);
-          });
-          setComments(commentsData);
-  
-          // Fetch class information here
-          // For example, assuming 'class' is the field containing the class information
-          const classSnapshot = await getDocs(collection(db, 'class'));
-          classSnapshot.forEach((doc) => {
-            setClassInfo(doc.data().class);
-          });
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      getData();
-    }, []);
-  
-    return (
-      <div className="course-home-page-container">
-        <h1 className="course-headline">{classInfo}</h1>
-        <div className="grid-of-posts">
-          {comments.map((comment, index) => (
-            <div key={index} className="post">
-              {comment}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const [comments, setComments] = useState([]);
+  const [classInfo, setClassInfo] = useState('');
+  const location = useLocation();
+  const { selectedCourse } = location.state || {};
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'classComments'));
+        const commentsData = [];
+        querySnapshot.forEach((doc) => {
+          commentsData.push(doc.data().Comment);
+        });
+        setComments(commentsData);
+
+        const classSnapshot = await getDocs(collection(db, 'class'));
+        classSnapshot.forEach((doc) => {
+          setClassInfo(doc.data().class);
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const addComment = async () => {
+    const newComment = window.prompt('Enter your comment:');
+    if (newComment) {
+      try {
+        const docRef = await addDoc(collection(db, 'classComments'), {
+          Comment: newComment,
+          // Add other fields as needed
+        });
+
+        setComments([...comments, newComment]);
+      } catch (error) {
+        console.error('Error adding document:', error);
+      }
+    }
   };
-  
-  export default CourseHomePage;
+
+  return (
+    <div className="course-home-page-container">
+      <h1 className="course-headline">{classInfo}</h1>
+      <div className="grid-of-posts">
+        {comments.map((comment, index) => (
+          <div key={index} className="post">
+            {comment}
+          </div>
+        ))}
+      </div>
+      <button onClick={addComment}>Add Comment</button>
+    </div>
+  );
+};
+
+export default CourseHomePage;
